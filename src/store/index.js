3d93a23d704f55ff64a575e1f8b201451e80b1a8
Chowdhury Nazmuz Sakib <nazmuz.sakib@brainstation-23.com>
2024-05-20 14:18:39 +0600

    conditonal chaining added using redux on the toggle button

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,25 @@
 import { createStore } from "redux";
-
-const stateCounter = (state = { counter: 0 }, action) => {
+const initialValues = { counter: 0, showCounter: true };
+const stateCounter = (state = initialValues, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   } else if (action.type === "increase") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   } else if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  } else if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   } else {
     return state;
